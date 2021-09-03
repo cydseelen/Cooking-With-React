0bea4c2b4394c598ebb1d979bf9f11ad025cc460
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-09-03 22:37:57 +0200

    Can now select a recipe and pass it to the edit section fields

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import RecipeIngredientEdit from './RecipeIngredientEdit'
 
-export default function RecipeEdit() {
+export default function RecipeEdit({ recipe }) {
     return (
         <div className="recipe-edit">
             <div className="recipe-edit__remove-button-container">
@@ -17,6 +17,7 @@ export default function RecipeEdit() {
                     type="text" 
                     name="name" 
                     id="name"
+                    value={recipe.name}
                     className="recipe-edit__input" />
                 <label 
                     htmlFor="cookTime"
@@ -26,7 +27,9 @@ export default function RecipeEdit() {
                 <input 
                     type="text" 
                     name="cookTime" 
-                    id="cookTime" />
+                    id="cookTime"
+                    value={recipe.cookTime}
+                    className="recipe-edit__input" />
                 <label 
                     htmlFor="servings"
                     className="recipe-edit__label">
@@ -36,7 +39,9 @@ export default function RecipeEdit() {
                     type="number" 
                     min="1"
                     name="servings" 
-                    id="servings" />
+                    id="servings"
+                    value={recipe.servings}
+                    className="recipe-edit__input" />
                 <label 
                     htmlFor="instructions"
                     className="recipe-edit__label">
@@ -46,6 +51,7 @@ export default function RecipeEdit() {
                     name="instructions" 
                     className="recipe-edit__input"
                     id="instructions">
+                    {recipe.instructions}
                 </textarea>
             </div>
             <br />
@@ -54,8 +60,12 @@ export default function RecipeEdit() {
                 <div>Name</div>
                 <div>Amount</div>
                 <div></div>
-                <RecipeIngredientEdit />
-                <RecipeIngredientEdit />
+                {recipe.ingredients.map(ingredient => (
+                    <RecipeIngredientEdit 
+                        key={ingredient.id} 
+                        ingredient={ingredient}
+                    />
+                ))}
             </div>
             <div className="recipe-edit__add-ingredient-btn-container">
                 <button className="btn btn--primary">Add Ingredient</button>
